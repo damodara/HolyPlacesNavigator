f6f285cc3c017004ebecf432323db3fe187315ab
damodara <jay108@mail.ru>
2021-07-12 17:58:23 +0300

    лаунч скрин

---
+++ Landmarks/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HP Navigator
+//
+//  Created by ddas on 12.07.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let imageView: 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
