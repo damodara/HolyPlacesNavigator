8d5eaf2e47dd4dd2642a9deb2b462e1ab39323cc
damodara <jay108@mail.ru>
2021-07-14 21:38:06 +0300

    изменил кнопку прокладывания маршрута и ее положение

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -12,33 +12,7 @@ struct LandmarkDetail: View {
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
             
-            Button (
-                action: {
-                    let latitude:CLLocationDegrees = self.landmark.locationCoordinate.latitude
-                    let longitude:CLLocationDegrees = self.landmark.locationCoordinate.longitude
-                
-                let regionDistance:CLLocationDistance = 1000;
-                let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
-                let regionSpan = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
-                
-                let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
-                
-                let placemark = MKPlacemark(coordinate: coordinates)
-                let mapItem = MKMapItem(placemark: placemark)
-                    mapItem.name = self.landmark.name
-                mapItem.openInMaps(launchOptions: options)
-            }) {
-                Image(systemName:"arrow.up.right.diamond")
-                    .renderingMode(.template)
-                    .imageScale(.large)
-                    .scaledToFill() // add if you need
-                    .frame(width: 50.0, height: 50.0) // as per your requirement
-                    .background(Color(UIColor.systemBackground))
-                    .cornerRadius(/*@START_MENU_TOKEN@*/14.0/*@END_MENU_TOKEN@*/)
-            }
-            .frame(width:UIScreen.main.bounds.width/2, height: UIScreen.main.bounds.height/2, alignment: .center)
-            .offset(x: 0, y: -350)
-            .padding(.bottom, -330)
+            
             
             
             
@@ -47,6 +21,38 @@ struct LandmarkDetail: View {
                 .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
 
+            Button (
+                    action: {
+                        let latitude:CLLocationDegrees = self.landmark.locationCoordinate.latitude
+                        let longitude:CLLocationDegrees = self.landmark.locationCoordinate.longitude
+                    
+                    let regionDistance:CLLocationDistance = 1000;
+                    let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+                    let regionSpan = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+                    
+                    let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+                    
+                    let placemark = MKPlacemark(coordinate: coordinates)
+                    let mapItem = MKMapItem(placemark: placemark)
+                        mapItem.name = self.landmark.name
+                    mapItem.openInMaps(launchOptions: options)
+                }) {
+                    Text("Get directions")
+                    Image(systemName:"arrow.up.right.diamond")
+                        .renderingMode(.template)
+                        .imageScale(.large)
+                        .scaledToFill() // add if you need
+                        .frame(width: 50.0, height: 50.0) // as per your requirement
+                        .background(Color(UIColor.systemBackground))
+                        .cornerRadius(/*@START_MENU_TOKEN@*/14.0/*@END_MENU_TOKEN@*/)
+            }
+            .shadow(radius: 5)
+            
+            
+                
+                
+                
+                
             VStack(alignment: .leading) {
                 Text(landmark.name)
                     .font(.title)
